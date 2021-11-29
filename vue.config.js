1827c1d84660f10d4a0b88e3e8e6ecf4370e14b1
Juan Jose <joel.10.gr@gmail.com>
2021-11-29 15:08:26 -0700

    desabilitando host

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ module.exports = {
   "transpileDependencies": [
     "bootstrap-vue / nuxt"
   ],
-  publicPath: '/apidomusweb',
   "runtimeCompiler": true,
   devServer: {
     'disableHostCheck': true
